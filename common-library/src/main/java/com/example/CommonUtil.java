10f45f9622eb057598e9e2602021cc7f51d14f74
MohammedAtif-BL <mohammed.atif@bridgelabz.com>
2025-02-26 12:53:38 +0530

    Version v1

--- common-library/src/main/java/com/example/CommonUtil.java
+++ common-library/src/main/java/com/example/CommonUtil.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class CommonUtil {
     public static void printMessage(String message) {
+        System.out.println("New Message for Version");
         System.out.println("CommonUtil: " + message);
     }
 }
